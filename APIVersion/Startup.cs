fb3f29aa7586f05b3341d5152c13c51e4fe16a5e
Code_hub <saumilsapariya@gmail.com>
2018-12-16 23:50:09 -0500

    Added health check

--- APIVersion/Startup.cs
+++ APIVersion/Startup.cs
@@ -30,6 +30,8 @@ namespace APIVersion
             {
                 o.AssumeDefaultVersionWhenUnspecified = true;
             });
+
+            services.AddHealthChecks();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
